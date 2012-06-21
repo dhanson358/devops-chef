c6724fe03163eafacbca91f61b17f190126adb4f
David Hanson <david@dhanson.org>
2012-06-21 11:31:42 -0500

    Update logrotated

--- cookbooks/logrotate/recipes/default.rb
+++ cookbooks/logrotate/recipes/default.rb
@@ -1,3 +1,13 @@
 package "logrotate" do
   action :upgrade
 end
+
+node[:logrotate][:services].each do |s|
+  cookbook_file "/etc/logrotate.d/#{s}" do
+    source s
+    mode "0644"
+    owner "root"
+    group "root"
+    backup false
+  end
+end
